7e73e44810bec27d694b3871d8882a5842af28af
zrz616 <zrz616@gmail.com>
2021-11-25 09:16:39 +0800

    <ci>: 添加deployments.yml

--- httpserver/main.go
+++ httpserver/main.go
@@ -23,7 +23,7 @@ func getClientIP(r *http.Request) string {
     return ""
 }
 
-func fooHandler(w http.ResponseWriter, r *http.Request) {
+func rootHandler(w http.ResponseWriter, r *http.Request) {
     glog.V(2).Info("fooHandler")
     glog.V(2).Info(r.Header)
     for k, v := range r.Header {
@@ -47,12 +47,15 @@ func healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     addr := flag.String("addr", ":8080", "specify the server binding address")
+    glogLevel := os.Getenv("GLOG_LEVEL")
+    flag.Set("v", glogLevel)
+    flag.Set("logtostderr", "true")
     flag.Parse()
     defer glog.Flush()
     // log.SetFlags(glog.Ldate | glog.Ltime)
     glog.V(1).Infof("listening %s\n", *addr)
     mux := http.NewServeMux()
-    mux.HandleFunc("/foo", fooHandler)
+    mux.HandleFunc("/", rootHandler)
     mux.HandleFunc("/healthz", healthcheckHandler)
     // glog.Fatal(http.ListenAndServe(*addr, nil))
     srv := &http.Server{
@@ -60,14 +63,14 @@ func main() {
         Handler: mux,
     }
 
+    quit := make(chan os.Signal, 1)
+    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
     go func() {
         if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
             glog.Fatal("Listen: %s\n", err)
         }
     }()
-
-    quit := make(chan os.Signal, 1)
-    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+    glog.V(1).Info("httpserver started")
     <-quit
     glog.V(1).Info("httpserver shutting down...")
 
